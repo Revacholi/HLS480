3f6384942f6f574319d96feb413d19b037d59d60
Revacholi <tzuheng_li@outlook.com>
2025-11-11 16:24:00 +0100

    11.11 ver

--- src/krnl_sobel.cpp
+++ src/krnl_sobel.cpp
@@ -33,77 +33,77 @@ void conv_3x3(gradient_stream& in, gradient_stream& out,
 
     //out.write(in.read()); 
 
-    // gradient_t line_buffer[2][IMG_WIDTH];
-    // #pragma HLS ARRAY_PARTITION variable=line_buffer dim=1 complete
+    gradient_t line_buffer[2][IMG_WIDTH];
+    #pragma HLS ARRAY_PARTITION variable=line_buffer dim=1 complete
     
-    // // 3x3 window
-    // gradient_t window[3][3];
-    // #pragma HLS ARRAY_PARTITION variable=window dim=0 complete
+    // 3x3 window
+    gradient_t window[3][3];
+    #pragma HLS ARRAY_PARTITION variable=window dim=0 complete
     
-    // // initialization
-    // for(int i = 0; i < 2; i++) {
-    //     for(int j = 0; j < IMG_WIDTH; j++) {
-    //         line_buffer[i][j] = 0;
-    //     }
-    // }
-    // for(int i = 0; i < 3; i++) {
-    //     for(int j = 0; j < 3; j++) {
-    //         window[i][j] = 0;
-    //     }
-    // }
+    // initialization
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < IMG_WIDTH; j++) {
+            line_buffer[i][j] = 0;
+        }
+    }
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            window[i][j] = 0;
+        }
+    }
     
-    // // deal with stream data with inf loop
-    // while(true) {
-    //     // whole frame
-    //     for(int row = 0; row < IMG_HEIGHT; row++) {
-    //         for(int col = 0; col < IMG_WIDTH; col++) {
-    //             #pragma HLS pipeline II=1
+    // deal with stream data with inf loop
+    while(true) {
+        // whole frame
+        for(int row = 0; row < IMG_HEIGHT; row++) {
+            for(int col = 0; col < IMG_WIDTH; col++) {
+                #pragma HLS pipeline II=1
                 
-    //             gradient_axis in_pixel = in.read();
-    //             gradient_t current_pixel = in_pixel.data;
+                gradient_axis in_pixel = in.read();
+                gradient_t current_pixel = in_pixel.data;
                 
-    //             // move window to left
-    //             for(int i = 0; i < 3; i++) {
-    //                 window[i][0] = window[i][1];
-    //                 window[i][1] = window[i][2];
-    //             }
+                // move window to left
+                for(int i = 0; i < 3; i++) {
+                    window[i][0] = window[i][1];
+                    window[i][1] = window[i][2];
+                }
                 
-    //             // fill the right col
-    //             window[0][2] = line_buffer[0][col];  
-    //             window[1][2] = line_buffer[1][col];  
-    //             window[2][2] = current_pixel;         
+                // fill the right col
+                window[0][2] = line_buffer[0][col];  
+                window[1][2] = line_buffer[1][col];  
+                window[2][2] = current_pixel;         
                 
-    //             // update line buffer
-    //             line_buffer[0][col] = line_buffer[1][col];
-    //             line_buffer[1][col] = current_pixel;
+                // update line buffer
+                line_buffer[0][col] = line_buffer[1][col];
+                line_buffer[1][col] = current_pixel;
                 
-    //             // cal conv
-    //             ap_int<20> sum = 0;
-    //             for(int i = 0; i < 3; i++) {
-    //                 for(int j = 0; j < 3; j++) {
+                // cal conv
+                ap_int<20> sum = 0;
+                for(int i = 0; i < 3; i++) {
+                    for(int j = 0; j < 3; j++) {
  
-    //                     int img_row = row - 1 + i;  
-    //                     int img_col = col - 1 + j;
+                        int img_row = row - 1 + i;  
+                        int img_col = col - 1 + j;
                         
-    //                     // edge check
-    //                     gradient_t pixel_val = 0;
-    //                     if(img_row >= 0 && img_row < IMG_HEIGHT && 
-    //                        img_col >= 0 && img_col < IMG_WIDTH) {
-    //                         pixel_val = window[i][j];
-    //                     }
+                        // edge check
+                        gradient_t pixel_val = 0;
+                        if(img_row >= 0 && img_row < IMG_HEIGHT && 
+                           img_col >= 0 && img_col < IMG_WIDTH) {
+                            pixel_val = window[i][j];
+                        }
                         
-    //                     sum += pixel_val * conv_kernel[i][j];
-    //                 }
-    //             }
+                        sum += pixel_val * conv_kernel[i][j];
+                    }
+                }
                 
-    //             gradient_axis out_pixel;
-    //             out_pixel.data = (gradient_t)sum;
-    //             out_pixel.user = in_pixel.user;  
-    //             out_pixel.last = in_pixel.last;  
-    //             out.write(out_pixel);
-    //         }
-    //     }
-    // }
+                gradient_axis out_pixel;
+                out_pixel.data = (gradient_t)sum;
+                out_pixel.user = in_pixel.user;  
+                out_pixel.last = in_pixel.last;  
+                out.write(out_pixel);
+            }
+        }
+    }
     
 }
 
